e4cadfc914d9d5849c5936ca77fe5bf5a7ab7e4f
Sanpreet Singh <sany26.com@gmail.com>
2019-10-08 18:12:52 +0530

    Updated views.py

--- relationaldatadjango/databaseapp/views.py
+++ relationaldatadjango/databaseapp/views.py
@@ -19,10 +19,20 @@ def index(request):
 	filtered_name_using_article_j=Reporter.objects.filter(article__headline="another heading by John Smith") 
 	list_name_j = list()
 	for name in filtered_name_using_article_j:
-		list_name_j.append(name.first_name + " " + name.last_name) 
+		list_name_j.append(name.first_name + " " + name.last_name)
 
-# context is used because jinja template needs a context to be diplayed
-# on the web page	
+	all_records_reporter = Reporter.objects.all()
+	""" fetch all the ids present in the table Reporter"""
+	list_reporter_id = list()
+	for reporter_id in all_records_reporter:
+		list_reporter_id.append(reporter_id.id) 
+	"""passing each_id to the another table as this is relational database"""
+	for each_id in list_reporter_id:
+		print(Article.objects.filter(reporter__pk=each_id))
+
+
+	# context is used because jinja template needs a context to be diplayed
+	# on the web page	
 	context = {
 		"articles_john": list_articles_john,
 		"articles_sanpreet" : list_article_sanpreet,
